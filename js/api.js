ba08884f4cc69f7ff1c352c70dcade5e92213f4a
zhanglei <zhanglei@iherogroup.com>
2019-08-09 09:17:22 +0800

    格式化

--- js/api.js
+++ js/api.js
@@ -2,7 +2,7 @@
 /* 所有ajax的代码写到这里 */
 axios.defaults.baseURL='http://localhost:10010/v1'
 // 设置AJAX超时时间
-axios.defaults.timeout = 3000
+axios.defaults.timeout = 30000
 // 设置提交数据时的格式
 axios.defaults.headers['Content-Type'] = 'application/json'
 
@@ -23,18 +23,42 @@ axios.interceptors.request.use(function (config) {
 
 //登录
 function getLogin(params){
-    return axios.post("/web-service/getLogin", params);
+    return axios.post("/admin-service/getLogin", params);
+}
+//从redis查询当前登录的管理员信息
+function GetUserByRedis(){
+    return axios.get("/admin-service/GetUserByRedis");
 }
 //查询会员数
 function GetUserList(){
-    return axios.get("/web-service/GetUserList");
+    return axios.get("/admin-service/GetUserList");
 }
 //查询记录数
 function GetRecords(){
-    return axios.get("/web-service/GetRecords");
+    return axios.get("/admin-service/GetRecords");
 }
 //查询异常数
 function GetAbnormals(){
-    return axios.get("/web-service/GetAbnormals");
+    return axios.get("/admin-service/GetAbnormals");
+}
+//查询所有订单
+function getTransferRecords(params){
+    return axios.post("/admin-service/getTransferRecords",params);
+}
+//查询所有用户
+function getBankUserList(params){
+    return axios.post("/admin-service/getBankUserList",params);
+}
+//启用(用户状态)
+function memberStart(userId){
+    return axios.get("/admin-service/memberStart/"+userId);
+}
+//停用(用户状态)
+function memberStop(userId){
+    return axios.get("/admin-service/memberStop/"+userId);
+}
+//退出
+function loginOut(userName){
+    return axios.get("/admin-service/loginOut/"+userName);
 }
 
